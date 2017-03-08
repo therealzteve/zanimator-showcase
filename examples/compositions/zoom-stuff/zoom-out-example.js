7a58199b5bb87d254829a4b1120eb05143e233da
Steven Wruk <swruk@abasag.intra>
2017-03-08 17:20:20 +0100

    - Fixed zoom out example

---
+++ examples/compositions/zoom-stuff/zoom-out-example.js
@@ -0,0 +1,30 @@
+'use strict';
+exports.create = function(zAnimator){
+  var startPoint = { 'x': 0, 'y': 0 };
+
+  var square = zAnimator.factory.square({squareShape: zAnimator.geometry.shapes.square({sidelength: 80}), color: '#000'});
+  var interval = zAnimator.interval({type: 'ms', ms: 1000});
+  var zoomOut = zAnimator.compositions.zoomStuff.zoomOut({subject: square, speed: interval});
+
+  var example = {
+    name: 'zoom out example',
+    controls: [
+      {'name': 'Start point',
+       'type': 'coordinates',
+       'ref': startPoint
+     }
+    ],
+    run: function(){
+
+      square.view.x = startPoint.x;
+      square.view.y = startPoint.y;
+      zAnimator.mainContainer.add(square);
+      zoomOut.start();
+    },
+    stop: function (){
+      zAnimator.mainContainer.remove(square);
+    }
+  };
+
+  return example;
+}
